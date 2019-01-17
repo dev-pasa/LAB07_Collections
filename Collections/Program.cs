7d50449c44c1b5e344e58970086d6f44de6c608f
dev-pasa <dev5@uw.edu>
2019-01-16 22:43:19 -0800

    need fix

---
+++ Collections/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using Collections;
+
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck<Card> myDeck = new Deck<Card>();
+            Card c1 = new Card(Suit.Club, Value.Ace);
+            Card c3 = new Card(Suit.Spades, Value.Two);
+            Card c4 = new Card(Suit.Hearts, Value.Four);
+            Card c5 = new Card(Suit.Diamonds, Value.Jack);
+            Card c6 = new Card(Suit.Diamonds, Value.King);
+            Card c2 = new Card(Suit.Spades, Value.Queen);
+            Card c7 = new Card(Suit.Club, Value.Eight);
+            Card c8 = new Card(Suit.Hearts, Value.Ace);
+            Card c9 = new Card(Suit.Hearts, Value.Four);
+            Card c10 = new Card(Suit.Club, Value.Five);
+
+            Card[] cards = new Card[] { c1, c2, c3, c4, c5, c6, c7, c8, c9, c10 };
+            for (int i = 0; i < cards.Length; i++)
+            {
+                myDeck.Add(cards[i]);
+            }
+
+            Console.WriteLine("The cards in your deck are: ");
+            PrintDeck(myDeck);
+
+
+            NextStep("Remove Club of Ace");
+            myDeck.Remove(c1);
+            PrintDeck(myDeck);
+
+            Card c11 = new Card(Suit.Hearts, Value.Queen);
+            NextStep("Add Queen of Hearts");
+            Console.WriteLine();
+            myDeck.Add(c11);
+            PrintDeck(myDeck);
+
+        }
+
+
+
+        public static void PrintDeck(Deck<Card> mydeck)
+        {
+
+            foreach (Card card in mydeck)
+            {
+
+                if (mydeck != null)
+                {
+                    Console.WriteLine($"{card.Value} of {card.Suit}");
+                }
+            }
+        }
+
+
+
+
+        public static void NextStep(string step)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"#####   Hit <Enter> to {step}   ##### ");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# LAB07-Collections
+#Learning Objectives
+Understand and implement enums in the programs and classes.
+Identify and implement generic collection.
+
+#Objective
+The objective is to create a game. In the card game, we add some cards in a deck. The user has the access to add or remove cards from the deck. 
+The user can then run the deal() method that distributes the cards amongst two players.
+
+##Visuals
+markup: ![initial view](/Assets/card1.jpg)
+markup: ![delete one card](/Assets/card2.jpg)
+markup: ![add a card](/Assets/card3.jpg)
+### Other details
+The are 4 functions in this game, that are add, remove, count and deal. The user will be provided directions as to what to do next. 
